57170d063ed17130ed509bb9dec68b0365bd9b0a
Christophe1 <harris.christophe@gmail.com>
2018-05-24 00:37:47 +0100

    filter is working now

--- main/java/com/example/chris/myapplication/MainActivity.java
+++ main/java/com/example/chris/myapplication/MainActivity.java
@@ -7,10 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Toast;
 
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.example.chris.myapplication.R;
 
 import org.apache.http.HttpEntity;
@@ -30,37 +36,16 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+  public static final String KEY_PHONENUMBER_USER = "phonenumberofuser";
+
   private AutoCompleteTextView userName;
 
-  private String jsonString = "{\n" +
-      "\t\"results\": [{\n" +
-      "\t\t\t\"id\": \"1\",\n" +
-      "\t\t\t\"name\": \"stocktaker\"\n" +
-      "\t\t}, {\n" +
-      "\t\t\t\"id\": \"2\",\n" +
-      "\t\t\t\"name\": \"stoolmaker\"\n" +
-      "\t\t}, {\n" +
-      "\t\t\t\"id\": \"3\",\n" +
-      "\t\t\t\"name\": \"statesman\"\n" +
-      "\t\t}, {\n" +
-      "\t\t\t\"id\": \"4\",\n" +
-      "\t\t\t\"name\": \"abacus maker\"\n" +
-      "\t\t},\n" +
-      "\t\t{\n" +
-      "\t\t\t\"id\": \"5\",\n" +
-      "\t\t\t\"name\": \"artist\"\n" +
-      "\t\t}, {\n" +
-      "\t\t\t\"id\": \"6\",\n" +
-      "\t\t\t\"name\": \"airplane repairman\"\n" +
-      "\t\t}, {\n" +
-      "\t\t\t\"id\": \"7\",\n" +
-      "\t\t\t\"name\": \"architect\"\n" +
-      "\t\t}\n" +
-      "\t]\n" +
-      "}";
+  private static final String jsonString = "http://www.populisto.com/suggestion.php";
 
   private ArrayList<User> userList;
 
@@ -69,56 +54,92 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    initView();
-
-    //extract the individual details in jsonString
-    userList = extractUser(jsonString);
-
-    //context, layout, list of users
-    UserAdapter userAdapter = new UserAdapter(this, R.layout.user_raw_layout, userList);
-
-    userName.setAdapter(userAdapter);
-    userName.setThreshold(1);
-    //userName.setHint("Country");
-
-  }
-
-  private void initView() {
-
     userName = (AutoCompleteTextView) findViewById(R.id.user_name);
 
     userList = new ArrayList<>();
 
+    StringRequest request = new StringRequest(Request.Method.GET, jsonString,
+        new Response.Listener<String>() {
+
+          @Override
+          public void onResponse(String response) {
+
+            Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+
+            try {
+
+              //extract the individual details in jsonString
+              userList = extractUser(response);
+
+            } catch (Exception e) {
+              e.printStackTrace();
+            }
+
+            Toast.makeText(getApplicationContext(), userList.toString(), Toast.LENGTH_LONG).show();
+
+            //context, layout, list of users
+            UserAdapter userAdapter = new UserAdapter(getApplicationContext(), R.layout.user_raw_layout, userList);
+
+            userName.setAdapter(userAdapter);
+            userName.setThreshold(1);
+
+          }
 
 
+        }, new Response.ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError error) {
+
+      }
+    })
+
+    {
+      @Override
+      //post info to php
+      protected Map<String, String> getParams() {
+        Map<String, String> params = new HashMap<String, String>();
+        //phoneNoofUser is the value we get from Android, the user's phonenumber.
+        //the key,KEY_PHONENUMBER_USER, is "phonenumberofuser". When we see "phonenumberofuser" in our php,
+        //put in phoneNoofUser
+        params.put(KEY_PHONENUMBER_USER, "+353872934480");
+        return params;
+
+      }
+    };
+
+
+    AppController.getInstance().addToRequestQueue(request);
   }
 
   //extract the individual details in jsonString
-  private ArrayList<User> extractUser(String jsonString) {
+  private ArrayList<User> extractUser(String response) {
 
     ArrayList<User> list = new ArrayList<>();
 
     try {
-      JSONObject rootJO = new JSONObject(jsonString);
+      JSONObject rootJO = new JSONObject(response);
 
       //break down the array in the JSON object
       JSONArray userJA = rootJO.getJSONArray("results");
-      Toast.makeText(MainActivity.this, userJA.toString(), Toast.LENGTH_LONG).show();
-
 
       for (int i = 0; i < userJA.length(); i++) {
 
         JSONObject jo = userJA.getJSONObject(i);
 
-        int id = jo.getInt("id");
-        String name = jo.getString("name");
+        int id = jo.getInt("cat_id");
+        String name = jo.getString("cat_name");
 
         //make a new user object
         User user = new User(id, name);
 
         //add the object to the list array
         list.add(user);
+
+        Log.e("Parse JSON", "id: " + id + " name: " + name);
       }
+
+      Toast.makeText(getApplicationContext(), userList.toString(), Toast.LENGTH_LONG).show();
+
 
     } catch (JSONException e) {
       e.printStackTrace();
@@ -129,24 +150,3 @@ public class MainActivity extends AppCompatActivity {
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
